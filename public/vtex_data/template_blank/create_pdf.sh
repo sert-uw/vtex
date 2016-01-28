81cdaece4a264cd81afafe9c81cced7361d98edd
sert <demo.sert@gmail.com>
2016-01-28 23:40:03 +0900

    change create_pdf

--- public/vtex_data/template_blank/create_pdf.sh
+++ public/vtex_data/template_blank/create_pdf.sh
@@ -7,8 +7,8 @@ rm ./images/*.xbb
 extractbb ./images/*.png
 extractbb ./images/*.jpg
 
-platex -kanji=utf8 document.tex
 platex -kanji=utf8 document.tex >> errors/error.err
+platex -kanji=utf8 document.tex
 dvipdfmx -f ptex-ipa.map document.dvi
 
 rm *.log *.lot *.lof *.aux *.toc *.dvi
